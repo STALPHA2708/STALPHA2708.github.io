d884d3b084a91f62235439729a5d5d5ec046e00c
Med BERRHAZI <118722359+STALPHA2708@users.noreply.github.com>
2024-10-29 04:55:17 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,8 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("");
-
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.init("public");
+        emailjs.sendForm('service_fte7loi', 'template_b8wk0g5', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
